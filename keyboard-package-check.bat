784139730d42494626e9a9b1d6997107c163c783
Lewis Newlands <123844871+Lewis418@users.noreply.github.com>
2024-04-24 23:11:11 +0100

    Update keyboard-package-check.bat

--- keyboard-package-check.bat
+++ keyboard-package-check.bat
@@ -1,22 +1,26 @@
 @echo off
 :start
-pip list | findstr "keyboard" > pip-list-keyboard-check.txt
+pip list > pip-list-keyboard-check.txt
 
 set FILE=pip-list-keyboard-check.txt
 set SEARCH_STRING=keyboard
+
+set "FOUND=0"
 
 for /f "usebackq tokens=*" %%a in ("%FILE%") do (
     echo %%a | findstr /i "%SEARCH_STRING%" >nul
     if not errorlevel 1 (
         echo Found "%SEARCH_STRING%" in file "%FILE%"
-		echo You do not need to install this package!. 
-		del pip-list-keyboard-check.txt
-		exit
-    ) else (
-        echo "%SEARCH_STRING%" not found in file "%FILE%"
-		echo Installing new package!.
-		pip install keyboard 
-		del pip-list-keyboard-check.txt
-		exit 
+        echo You do not need to install this package!. 
+        set "FOUND=1"
     )
 )
+
+if "%FOUND%"=="0" (
+    echo "%SEARCH_STRING%" not found in file "%FILE%"
+    echo Installing new package!.
+    pip install keyboard 
+)
+del pip-list-keyboard-check.txt
+exit
+
